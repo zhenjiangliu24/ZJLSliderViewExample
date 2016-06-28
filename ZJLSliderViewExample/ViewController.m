26d3a799bc6ab937092c57c3fafee43349bf0178
lovehuahua24 <lovehuahua24@gmail.com>
2016-06-28 18:54:20 -0400

    code finished

--- ZJLSliderViewExample/ViewController.m
+++ ZJLSliderViewExample/ViewController.m
@@ -20,10 +20,23 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSString *url = @"https://img1.doubanio.com/view/movie_poster_cover/spst/public/p2339827309.jpg";
+    NSString *url2 = @"https://img3.doubanio.com/view/movie_poster_cover/spst/public/p2352310242.jpg";
+    NSString *url3 = @"https://img1.doubanio.com/view/movie_poster_cover/spst/public/p2345947329.jpg";
+    NSString *url4 = @"https://img3.doubanio.com/view/movie_poster_cover/spst/public/p2349374680.jpg";
     TestObject *obj = [[TestObject alloc] init];
     obj.imageURL = url;
     obj.imageName = @"test";
-    NSArray *testArray = @[obj];
+    TestObject *obj2 = [[TestObject alloc] init];
+    obj2.imageURL = url2;
+    obj2.imageName = @"name2";
+    TestObject *obj3 = [[TestObject alloc] init];
+    obj3.imageURL = url3;
+    obj3.imageName = @"name3";
+    TestObject *obj4 = [[TestObject alloc] init];
+    obj4.imageURL = url4;
+    obj4.imageName = @"name2";
+    
+    NSArray *testArray = @[obj,obj2,obj3,obj4];
     ZJLSliderView *sliderView = [[ZJLSliderView alloc] initWithFrame:CGRectMake(0, 50, self.view.frame.size.width, 125) data:testArray currentIndex:0];
     [self.view addSubview:sliderView];
 }
--- ZJLSliderViewExample/ZJLSliderView/ZJLSliderView.m
+++ ZJLSliderViewExample/ZJLSliderView/ZJLSliderView.m
@@ -49,6 +49,67 @@
     return [self initWithFrame:frame data:dataArray currentIndex:0];
 }
 
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    [self updateAllSubImageView];
+}
+
+- (void)updateAllSubImageView
+{
+    NSInteger index = (_mainScrollView.contentOffset.x+kScrollViewContentOffset)/(kItemSpacing+kItemWidth);
+    index = MIN(_dataArray.count-1, MAX(0, index));
+    CGFloat scale = (_mainScrollView.contentOffset.x+kScrollViewContentOffset-(kItemWidth+kItemSpacing)*index)/(kItemSpacing+kItemWidth);
+    if (_dataArray.count>0) {
+        CGFloat width;
+        CGFloat height;
+        if (scale<0.0) {//scroll to the most left
+            scale = 1-MIN(1.0, ABS(scale));
+            UIImageView *leftView = self.imageViewArrays[index];
+            leftView.layer.borderColor = [UIColor colorWithWhite:1 alpha:scale].CGColor;
+            height = kItemHeight + (kSelectedHeight - kItemHeight) * scale;
+            width = kItemWidth + (kSelectedWidth - kItemWidth) * scale;
+            leftView.frame = CGRectMake(-(width - kItemWidth) / 2, -(height - kItemHeight), width, height);
+            
+            if (index + 1 < self.dataArray.count) {
+                UIImageView *rightView = self.imageViewArrays[index + 1];
+                rightView.frame = CGRectMake(0, 0, kItemWidth, kItemHeight);
+                rightView.layer.borderColor = [UIColor clearColor].CGColor;
+            }
+        }else if (scale<=1.0){
+            if (index>=self.dataArray.count-1) {//scroll to the most right
+                scale = 1 - MIN(1.0, ABS(scale));
+                
+                UIImageView *imgView = self.imageViewArrays[self.dataArray.count - 1];
+                imgView.layer.borderColor = [UIColor colorWithWhite:1 alpha:scale].CGColor;
+                height = kItemHeight + (kSelectedHeight - kItemHeight) * scale;
+                width = kItemWidth + (kSelectedWidth - kItemWidth) * scale;
+                imgView.frame = CGRectMake(-(width - kItemWidth) / 2, -(height - kItemHeight), width, height);
+            }else{
+                CGFloat scaleLeft = 1 - MIN(1.0, ABS(scale));
+                UIImageView *leftView = self.imageViewArrays[index];
+                leftView.layer.borderColor = [UIColor colorWithWhite:1 alpha:scaleLeft].CGColor;
+                height = kItemHeight + (kSelectedHeight - kItemHeight) * scaleLeft;
+                width = kItemWidth + (kSelectedWidth - kItemWidth) * scaleLeft;
+                leftView.frame = CGRectMake(-(width - kItemWidth) / 2, -(height - kItemHeight), width, height);
+                
+                CGFloat scaleRight = MIN(1.0, ABS(scale));
+                UIImageView *rightView = self.imageViewArrays[index + 1];
+                rightView.layer.borderColor = [UIColor colorWithWhite:1 alpha:scaleRight].CGColor;
+                height = kItemHeight + (kSelectedHeight - kItemHeight) * scaleRight;
+                width = kItemWidth + (kSelectedWidth - kItemWidth) * scaleRight;
+                rightView.frame = CGRectMake(-(width - kItemWidth) / 2, -(height - kItemHeight), width, height);
+            }
+        }
+        for (UIImageView *imageView in self.imageViewArrays) {
+            if (imageView.tag != index+kBaseTag && imageView.tag != index+kBaseTag+1) {
+                imageView.frame = CGRectMake(0, 0, kItemWidth, kItemHeight);
+                imageView.layer.borderColor = [UIColor clearColor].CGColor;
+            }
+        }
+    }
+}
+
 #pragma mark - set current offset with index
 - (void)setWithCurrentIndex:(NSInteger)index
 {
@@ -80,12 +141,15 @@
     _imageViewArrays = [NSMutableArray array];
     NSInteger index = 0;
     for (id data in _dataArray) {
-        UIImageView *subView = [[UIImageView alloc] initWithFrame:CGRectMake((kItemWidth+kItemSpacing)*index+kItemSpacing, self.frame.size.height-kItemHeight, kItemWidth, kItemHeight)];
+        UIView *container = [[UIView alloc] initWithFrame:CGRectMake((kItemWidth+kItemSpacing)*index+kItemSpacing, self.frame.size.height-kItemHeight, kItemWidth, kItemHeight)];
+        [_mainScrollView addSubview:container];
+        
+        UIImageView *subView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, kItemWidth, kItemHeight)];
         subView.userInteractionEnabled = YES;
         subView.backgroundColor = [UIColor blackColor];
         subView.layer.borderWidth = 1.0;
         subView.tag = kBaseTag+index;
-        [_mainScrollView addSubview:subView];
+        [container addSubview:subView];
         [_imageViewArrays addObject:subView];
         if ([data isKindOfClass:[UIImage class]]) {
             subView.image = (UIImage *)data;
@@ -155,5 +219,46 @@
         }
     }
     
+}
+
+#pragma mark - scroll view delegate methods
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    NSInteger index = (scrollView.contentOffset.x + kScrollViewContentOffset + (kItemWidth / 2 + kItemSpacing / 2)) / (kItemWidth + kItemSpacing);
+    index = MIN(self.dataArray.count - 1, MAX(0, index));
+    _currentIndex = index;
+    [self updateAllSubImageView];
+}
+
+- (void)scrollViewWillEndDragging:(UIScrollView *)scrollView withVelocity:(CGPoint)velocity targetContentOffset:(inout CGPoint *)targetContentOffset
+{
+    NSInteger index = (targetContentOffset->x + kScrollViewContentOffset + (kItemWidth / 2 + kItemSpacing / 2)) / (kItemWidth + kItemSpacing);
+    targetContentOffset->x = (kItemSpacing + kItemWidth) * index - kScrollViewContentOffset;
+}
+
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView
+{
+    if (self.currentIndex<self.dataArray.count) {
+        [self updateBackgroundImageView];
+        CATransition *transition = [CATransition animation];
+        transition.duration = 0.6f;
+        transition.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+        transition.type = kCATransitionFade;
+        [self.backgroundImageView.layer addAnimation:transition forKey:nil];
+    }
+}
+
+- (void)scrollViewDidEndScrollingAnimation:(UIScrollView *)scrollView
+{
+    if (!CGPointEqualToPoint(self.offset, scrollView.contentOffset)) {
+        [self.mainScrollView setContentOffset:self.offset animated:YES];
+    }else{
+        [self updateBackgroundImageView];
+        CATransition *transition = [CATransition animation];
+        transition.duration = 0.6f;
+        transition.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+        transition.type = kCATransitionFade;
+        [self.backgroundImageView.layer addAnimation:transition forKey:nil];
+    }
 }
 @end
